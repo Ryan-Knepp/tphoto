66e7533e0e7c0743f76136da53c8b057986bc557
Ryan Knepp <ryan@mirus.io>
2021-08-31 21:28:58 -0500

    styling for services section

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,68 +1,96 @@
 <template>
   <Layout>
     <Collage />
-    <h1>
+    <!-- <h1>
       Available Packages
-    </h1>
-    <div class="flex w-full gap-1 h-1/2">
-      <section class="w-1/4 flex flex-col">
-        <h2 class="text-2xl">
+    </h1> -->
+    <div class="flex flex-col w-full gap-8 mt-8">
+      <ServiceCard>
+        <template #title>
           Session Series
-        </h2>
-        <g-image src="../assets/packages/series.jpg" class="" />
-        <p class="flex-grow flex-shrink-0">
+        </template>
+        <template #image>
+          <g-image
+            src="../assets/packages/series.jpg"
+            class="object-scale-down"
+          />
+        </template>
+        <template #body>
           For the Mom who wants to capture several sessions in a 2 year period -
           this is the best value!
-        </p>
-        <p class="pt-4">
+        </template>
+        <template #price>
           $300 / session (4 sessions in 2 years)
-        </p>
-      </section>
-      <section class="w-1/4 flex flex-col">
-        <h2 class="text-2xl">
+        </template>
+      </ServiceCard>
+      <ServiceCard>
+        <template #title>
           Portrait Session
-        </h2>
-        <g-image src="../assets/packages/portrait.jpg" class="" />
-        <p class="flex-grow flex-shrink-0">
+        </template>
+        <template #image>
+          <g-image
+            src="../assets/packages/portrait.jpg"
+            class="object-scale-down"
+          />
+        </template>
+        <template #body>
           For the Mom who would love pictures of her kid(s) (or family!) to
           treasure & display in her home. With genuine personality &
           relationship represented.
-        </p>
-        <p class="pt-4">Starts at $300</p>
-      </section>
-      <section class="w-1/4 flex flex-col">
-        <h2 class="text-2xl">
+        </template>
+        <template #price>
+          Starts at $300
+        </template>
+      </ServiceCard>
+      <ServiceCard>
+        <template #title>
           Newborn Session
-        </h2>
-        <g-image src="../assets/packages/newborn.jpg" class="" />
-        <p class="flex-grow flex-shrink-0">
+        </template>
+        <template #image>
+          <g-image
+            src="../assets/packages/newborn.jpg"
+            class="object-scale-down"
+          />
+        </template>
+        <template #body>
           For the Mom who wants to forever remember what your little looked like
           during that first week of life. The tiny hands, the sweet faces, the
           perfect details.
-        </p>
-        <p class="pt-4">Starts at $300</p>
-      </section>
-      <section class="w-1/4 flex flex-col">
-        <h2 class="text-2xl">
+        </template>
+        <template #price>
+          Starts at $300
+        </template>
+      </ServiceCard>
+      <ServiceCard>
+        <template #title>
           Senior Session
-        </h2>
-        <g-image src="../assets/packages/senior.jpg" class="" />
-        <p class="flex-grow flex-shrink-0">
+        </template>
+        <template #image>
+          <g-image
+            src="../assets/packages/senior.jpg"
+            class="object-scale-down"
+          />
+        </template>
+        <template #body>
           For the Mom who has a graduating senior-- how did time go by so
           quickly?! A way to cement in this exciting milestone in their life &
           express their personality & passions!
-        </p>
-        <p class="pt-4">Starts at $350</p>
-      </section>
+        </template>
+        <template #price>
+          Starts at $350
+        </template>
+      </ServiceCard>
     </div>
   </Layout>
 </template>
 
 <script>
 import Collage from "../components/Collage.vue";
+import ServiceCard from "../components/ServiceCard.vue";
 export default {
   components: {
     Collage,
+    ServiceCard,
   },
   setup() {
     return {};
