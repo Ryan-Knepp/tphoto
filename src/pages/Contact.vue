8bef73e48797f545dbc775be9fdb0c472c7b6878
Ryan Knepp <ryan@mirus.io>
2021-10-30 21:39:54 -0500

    add loading spinner for typeform

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,36 @@
 <template>
   <Layout>
-    <div ref="contactForm" class="mb-8 mx-2 xl:mx-0 contact-form" />
+    <div class="contact-form">
+      <div
+        v-if="!this.isLoaded"
+        class="h-full w-full flex justify-center items-center"
+      >
+        <svg
+          class="animate-spin -ml-1 mr-3 h-16 w-16 text-t-dark-pink"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+      <div
+        ref="contactForm"
+        :class="`${this.isLoaded ? '' : 'hidden'} pb-4 mx-2 xl:mx-0 h-full`"
+      />
+    </div>
   </Layout>
 </template>
 
@@ -9,15 +39,24 @@ import { createWidget } from "@typeform/embed";
 import "@typeform/embed/build/css/widget.css";
 
 export default {
+  data: () => {
+    return {
+      isLoaded: false,
+    };
+  },
   mounted: function() {
     createWidget("MVIeXb25", {
       container: this.$refs.contactForm,
       inlineOnMobile: true,
+      onReady: this.setIsLoaded,
     });
   },
   methods: {
     toggleSlider: function() {
       this.slider.toggle();
+    },
+    setIsLoaded: function() {
+      this.isLoaded = true;
     },
   },
 };
